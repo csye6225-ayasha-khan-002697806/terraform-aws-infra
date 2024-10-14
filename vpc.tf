70c29bf9328e00c2a35f263ad0fcd4d0ac0d0a2c
Ayasha Khan <144647351+KhanAyasha@users.noreply.github.com>
2024-10-14 18:22:21 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "csye6225_vpc" {
 
 # # Create VPC
 # resource "aws_vpc" "my_vpc" {
-#   cidr_block = var.vpc_cidr
+#   cidr_block =      var.vpc_cidr
 #   tags = {
 #     Name = "MyVPC"
 #   }
