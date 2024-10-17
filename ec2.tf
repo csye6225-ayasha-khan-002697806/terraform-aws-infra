8c88198d87c1b5e0247d6252666cc2ec556f7ced
KhanAyasha <khan.ayas@northeastern.edu>
2024-10-17 14:07:48 -0400

    resolved variable error

--- ec2.tf
+++ ec2.tf
@@ -9,8 +9,8 @@ resource "aws_instance" "csye6225_ec2_instance" {
 
   ami           = var.ami_id
   instance_type = var.instance_type
-  #   subnet_id              = element(aws_subnet.dev_public_subnet.*.id, 0)
-  subnet_id              = aws_subnet.dev_public_subnet[0].id
+  subnet_id     = element(aws_subnet.dev_public_subnet.*.id, 0)
+  # subnet_id              = aws_subnet.dev_public_subnet[0].id
   vpc_security_group_ids = [aws_security_group.csye6225_security_group.id]
   #   security_group_ids = [aws_security_group.csye6225_security_group.id]
 
