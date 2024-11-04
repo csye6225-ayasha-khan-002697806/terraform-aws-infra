1da3d745908ba4f558bb2a2f36ff5e01b5613198
KhanAyasha <khan.ayas@northeastern.edu>
2024-11-04 02:17:31 -0500

    updated s3.tf
    separated s3 creation code with iam roles and policy

--- s3.tf
+++ s3.tf
@@ -7,55 +7,8 @@ resource "aws_s3_bucket" "s3_bucket" {
   force_destroy = true
 }
 
-# IAM Role and Policy for S3 and CloudWatch
-resource "aws_iam_role" "ec2_role" {
-  name = "IAMRoleForEc2"
-  assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Principal" : {
-          "Service" : "ec2.amazonaws.com"
-        },
-        "Action" : "sts:AssumeRole"
-      }
-    ]
-  })
-}
 
-# Policy for managing the S3 bucket
-resource "aws_iam_policy" "s3_bucket_policy" {
-  name        = "S3BucketPolicy"
-  description = "Policy for managing the S3 bucket"
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:DeleteObject",
-          "s3:ListBucket",
-          "s3:PutBucketEncryption",
-          "s3:PutLifecycleConfiguration"
-        ],
-        "Resource" : [
-          "arn:aws:s3:::${aws_s3_bucket.s3_bucket.bucket}",  # Correctly reference the bucket name
-          "arn:aws:s3:::${aws_s3_bucket.s3_bucket.bucket}/*" # Access to all objects in the bucket
-        ]
-      }
-    ]
-  })
-}
-
-# Attach the policy to the role
-resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.s3_bucket_policy.arn
-}
-
+# Block public access settings for the S3 bucket
 resource "aws_s3_bucket_public_access_block" "s3_private_bucket" {
   bucket                  = aws_s3_bucket.s3_bucket.id
   block_public_acls       = true
@@ -79,6 +32,8 @@ resource "aws_s3_bucket_lifecycle_configuration" "private_bucket_lifecycle" {
   }
 }
 
+
+# Server-side encryption configuration for S3 bucket
 resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encrypt" {
   bucket = aws_s3_bucket.s3_bucket.id
   rule {
@@ -86,47 +41,4 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "s3_encrypt" {
       sse_algorithm = "AES256"
     }
   }
-}
-
-# Attach CloudWatch policies to the IAM role
-data "aws_iam_policy" "cloudwatch_agent_policy" {
-  arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-
-# Attach the IAM policy to the IAM role
-resource "aws_iam_role_policy_attachment" "attach_cloudwatch_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = data.aws_iam_policy.cloudwatch_agent_policy.arn
-}
-
-resource "aws_iam_policy" "statsd_cloudwatch_policy" {
-  name        = "StatsDCloudWatchPolicy"
-  description = "Policy for publishing custom StatsD metrics to CloudWatch"
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "cloudwatch:PutMetricData"
-        ],
-        "Resource" : "*",
-        "Condition" : {
-          "StringEquals" : {
-            "cloudwatch:namespace" : "StatsD"
-          }
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "attach_statsd_policy" {
-  role       = aws_iam_role.ec2_role.name
-  policy_arn = aws_iam_policy.statsd_cloudwatch_policy.arn
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile"
-  role = aws_iam_role.ec2_role.name
 }
